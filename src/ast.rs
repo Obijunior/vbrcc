6dfe1c228da19a8c0fd0221cb121140d86a8ed1d
Henry Nwagwu <obijrnwagwu@gmail.com>
2026-07-20 12:34:44 -0500

    added Type enum with loose sizing

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,33 @@
 use crate::diagnostic::{Span, Spanned};
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum Type {
+    Int,
+    Char,
+    Long,
+    Void,
+    Pointer(Box<Type>),
+    Array(Box<Type>, usize),
+    Unknown,
+}
+
+impl Type {
+    pub fn size(&self) -> usize {
+        match self {
+            Type::Array(elem, len) => elem.size() * len,
+            _ => 8,
+        }
+    }
+
+    /// Loose alignment: 8 for scalars, element alignment for arrays.
+    pub fn align(&self) -> usize {
+        match self {
+            Type::Array(elem, _) => elem.align(),
+            _ => 8,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Expr {
     IntLiteral(i64),
@@ -59,4 +87,28 @@ pub struct Function {
 #[derive(Debug, PartialEq)]
 pub struct Program {
     pub functions: Vec<Function>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn loose_sizes_are_eight_for_scalars() {
+        assert_eq!(Type::Int.size(), 8);
+        assert_eq!(Type::Char.size(), 8);
+        assert_eq!(Type::Long.size(), 8);
+        assert_eq!(Type::Pointer(Box::new(Type::Int)).size(), 8);
+    }
+
+    #[test]
+    fn array_size_is_element_times_len() {
+        assert_eq!(Type::Array(Box::new(Type::Int), 10).size(), 80);
+    }
+
+    #[test]
+    fn scalar_align_is_eight() {
+        assert_eq!(Type::Int.align(), 8);
+        assert_eq!(Type::Pointer(Box::new(Type::Char)).align(), 8);
+    }
 }
